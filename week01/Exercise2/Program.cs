dc36a1f0a2a1e721403b98edac7a7bd5ba63631b
berioni7 <gberioni@byupathway.edu>
2025-03-03 20:37:50 -0300

    added if statment

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your grade percentage: ");
+        string percentage = Console.ReadLine();
+        int numero = int.Parse(percentage);
+
+        if (numero >= 90)
+        {
+            Console.WriteLine("You have passed in this class with an A. Congrats!");
+        }
+        else if (numero >= 80)
+        {
+            Console.WriteLine("You have passed in this class with a B. Congrats!");
+        }
+        else if (numero >= 70)
+        {
+            Console.WriteLine("You have passed in this class with a C. Congrats!");
+        }
+        else if (numero >= 60)
+        {
+            Console.WriteLine("You have passed in this class with a D. Congrats!");
+        }
+        else
+        {
+            Console.WriteLine("You have not passed in this class. You got an F. Please, keep studying and see you next class!");
+        }
     }
 }
