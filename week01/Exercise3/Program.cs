dd9b00b7fec06cc65be141e9ca0744d39a35e090
berioni7 <gberioni@byupathway.edu>
2025-03-03 22:09:36 -0300

    Added a loop and if statments

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int magic = 6;
+        Console.WriteLine("What is the magic number(1-10)? ");
+        string input = Console.ReadLine();
+        int numbers = int.Parse(input);
+        while (numbers != magic)
+        {
+            Console.WriteLine("Try again: ");
+            numbers = int.Parse(Console.ReadLine());
+            if (numbers == magic)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (numbers == 5)
+            {
+            Console.WriteLine("Higher");
+            }
+            else if (numbers == 4)
+            {
+            Console.WriteLine("Higher");
+            }
+            else if (numbers == 3)
+            {
+            Console.WriteLine("Higher");
+            }
+            else if (numbers == 2)
+            {
+            Console.WriteLine("Higher");
+            }
+            else if (numbers == 1)
+            {
+            Console.WriteLine("Higher");
+            }
+            else
+            {
+            Console.WriteLine("Lower");
+            }
+        }
     }
 }
