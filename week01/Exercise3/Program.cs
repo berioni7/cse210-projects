1b2abe6123871029a7f9bc0b3625af02bfe619c5
berioni7 <gberioni@byupathway.edu>
2025-03-04 22:48:20 -0300

    Added functions

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -40,7 +40,7 @@ class Program
             else
             {
             Console.WriteLine("Lower");
-            }
+            } 
         }
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        void PromptUserName()
+        {
+            Console.WriteLine("Write your User's name");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Welcome {userName}");
+        }
+
+        int PromptUserNumber()
+        {
+            Console.WriteLine("What is your favorite number?");
+            string numbers = Console.ReadLine();
+            int userNumber = int.Parse(numbers);
+            return userNumber;
+        }
+
+        int SquareNumber(int number)
+        {
+            return number * number;
+        }
+
+        void DisplayResult(string userName, int squaredNumber)
+        {
+            Console.WriteLine($"Hello {userName}, your favorite number squared is: {squaredNumber}");
+        }
+
+        DisplayWelcome();
+        PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult("User", squaredNumber);  
     }
 }
