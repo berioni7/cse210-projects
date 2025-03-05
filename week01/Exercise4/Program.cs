8e484beed41bd44a1605f7b5fc9d49459a518372
berioni7 <gberioni@byupathway.edu>
2025-03-04 21:21:58 -0300

    Added a list

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+            List<int> numbers = new List<int>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.Write("Write one number: ");
+            int number;
+            
+          
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.Write("Invalid answer. Provide a correct number: ");
+            }
+
+            numbers.Add(number);
+        }
+
+        int sum = numbers.Sum();
+        Console.WriteLine("\nThe total is: " + sum);
+
+        double average = numbers.Average();
+        Console.WriteLine("The average is: " + average.ToString("F2")); 
+
+        int maxNumber = numbers.Max();
+        Console.WriteLine("The largest number is: " + maxNumber);
     }
 }
